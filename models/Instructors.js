be2c9ddab6e17f63274414405799efe6a6d4bb80
Delakwebtech <79271954+Delakwebtech@users.noreply.github.com>
2025-05-16 09:19:08 +0100

    refactor: change instructorId type from UUID to INTEGER and add password hashing hooks

--- models/Instructors.js
+++ models/Instructors.js
@@ -1,10 +1,11 @@
 const { DataTypes } = require("sequelize");
+const bcrypt = require("bcryptjs");
 const sequelize = require("../config/db");
 
 const Instructor = sequelize.define("Instructor", {
     instructorId: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true,
     },
     fullName: {
@@ -26,6 +27,19 @@ const Instructor = sequelize.define("Instructor", {
             }
         }
     },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Please provide a password'
+            },
+            len: {
+                args: [6],
+                msg: 'Password should be at least 6 characters long'
+            }
+        }
+    },
     email: {
         type: DataTypes.STRING,
         allowNull: true,
@@ -35,7 +49,37 @@ const Instructor = sequelize.define("Instructor", {
         type: DataTypes.ENUM('active', 'inactive'),
         defaultValue: 'active',
     },
+}, {
+    hooks: {
+        beforeCreate: async (instructor) => {
+            if (instructor.password) {
+                const salt = await bcrypt.genSalt(10);
+                instructor.password = await bcrypt.hash(instructor.password, salt);
+            }
+        },
+        beforeUpdate: async (instructor) => {
+            if (instructor.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                instructor.password = await bcrypt.hash(instructor.password, salt);
+            }
+        },
+        afterCreate: async (instructor, options) => {
+            const { User } = require("./User"); // dynamically require to avoid circular import
+            await User.create({
+                fullName: instructor.fullName,
+                email: instructor.email,
+                phoneNumber: instructor.phoneNumber,
+                password: instructor.password, // already hashed
+                role: "instructor",
+            });
+        }
+    }
 });
+
+// Password validation method
+Instructor.prototype.validatePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 Instructor.associate = (models) => {
     Instructor.hasMany(models.Customer, {
@@ -43,10 +87,5 @@ Instructor.associate = (models) => {
         as: 'customers',
     });
 };
-
-// Instructor.hasMany(models.Customer, {
-//     foreignKey: 'assignedInstructorId',
-//     as: 'customers',
-// });
 
 module.exports = Instructor;
